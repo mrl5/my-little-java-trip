8115f75e3071682443666e3d0a9162d65df10dfa
mrl5 <jakuboo+github@gmail.com>
2017-09-09 18:08:07 +0200

    new Rounding class

---
+++ main/src/com/tuxnet/Rounding.java
@@ -0,0 +1,26 @@
+package com.tuxnet;
+
+import java.text.DecimalFormat;
+
+public class Rounding {
+    private String format = "0.";
+
+    private String calc(double number) {
+        DecimalFormat df = new DecimalFormat(format);
+        return df.format(number);
+    }
+
+    public String round(double number, int precision) {
+        String output = "";
+        try {
+            for (int i = 1; i < precision; i++) {
+                format += "#";
+            }
+
+            output = calc(number);
+        } catch (IllegalArgumentException e) {
+            System.out.println("Rounding precision parameter must fulfil the condition: \"precision >= 0\"");
+        }
+        return output;
+    }
+}
